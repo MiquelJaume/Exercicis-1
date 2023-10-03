841571297a261e3b1d6139e988de3668e6e4a771
brezhy <127342001+brezhy@users.noreply.github.com>
2023-10-03 18:59:46 +0200

    Programa parell o senar (ara amb scanner)

---
+++ src/ExercicisMultiproces1.java
@@ -0,0 +1,2 @@
+public class ExercicisMultiproces1 {
+}
--- src/ExercicisMultiproces1_ParellSenar.java
+++ src/ExercicisMultiproces1_ParellSenar.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class ExercicisMultiproces1_ParellSenar {
     public static void main(String[] args) {
-        int nombre = 8;
-        if ((nombre % 2) == 0) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introdueix un nombre: ");
+        int nombre = scanner.nextInt();
+
+        if (nombre % 2 == 0) {
             System.out.println("El nombre " + nombre + " és parell");
         } else {
             System.out.println("El nombre " + nombre + " és senar");
         }
+
+        scanner.close();
     }
 }
