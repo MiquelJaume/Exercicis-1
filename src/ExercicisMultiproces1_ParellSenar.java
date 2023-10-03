b8c673aabf28e03d717d9549622470474ea2c2dd
brezhy <127342001+brezhy@users.noreply.github.com>
2023-10-03 18:29:58 +0200

    Programa parell o senar

---
+++ src/ExercicisMultiproces1_ParellSenar.java
@@ -0,0 +1,10 @@
+public class ExercicisMultiproces1_ParellSenar {
+    public static void main(String[] args) {
+        int nombre = 8;
+        if ((nombre % 2) == 0) {
+            System.out.println("El nombre " + nombre + " és parell");
+        } else {
+            System.out.println("El nombre " + nombre + " és senar");
+        }
+    }
+}
